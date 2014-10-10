e8f17658d9ef8e50148c5876197adc883ebdc2e4
Matthias Lübken <matthias@dockzero.io>
2014-10-10 16:10:38 +0200

    msg

--- src/main/java/sparkexample/Hello.java
+++ src/main/java/sparkexample/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
     public static void main(String[] args) {
         get("/", (req, res) -> {
-            return "hello world from spark";
+            return "hello from sparkjava.com";
         });
     }
 }
